13fad1b894c83183229f53022d668007d2c8bf6d
parastoonozar <148582607+parastoonozar@users.noreply.github.com>
2024-01-01 18:13:39 +0330

    Add files via upload

---
+++ Code_010_LM35/Code_010/Code_010.ino
@@ -0,0 +1,19 @@
+
+const int analogInPin = A0;
+float AValue = 0;
+float AValue2 = 0;
+void setup() {
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  //AValue = map(analogRead(analogInPin), 0, 1023, 0, 5);
+  AValue = analogRead(analogInPin) * 0.0048828125 ; //((float)(5/1024));
+  AValue2 = analogRead(analogInPin) * 0.48828125 ; //((float)(5/1024));
+  Serial.print("Voltage = ");
+  Serial.print(AValue);
+  Serial.print("  Temp = ");
+  Serial.println(AValue2);
+  delay(100);
+}
