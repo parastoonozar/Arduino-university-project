19269a3f4e09ff59d45c9d07e640af90ab72fec5
parastoonozar <148582607+parastoonozar@users.noreply.github.com>
2024-01-01 18:14:02 +0330

    Add files via upload

---
+++ Code_011_LCD_Character/LCD_Character/LCD_Character.ino
@@ -0,0 +1,172 @@
+/*
+  LiquidCrystal Library - Custom Characters
+
+ Demonstrates how to add custom characters on an LCD  display.
+ The LiquidCrystal library works with all LCD displays that are
+ compatible with the  Hitachi HD44780 driver. There are many of
+ them out there, and you can usually tell them by the 16-pin interface.
+
+ This sketch prints "I <heart> Arduino!" and a little dancing man
+ to the LCD.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K potentiometer:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+ * 10K poterntiometer on pin A0
+
+ created 21 Mar 2011
+ by Tom Igoe
+ modified 11 Nov 2013
+ by Scott Fitzgerald
+
+ Based on Adafruit's example at
+ https://github.com/adafruit/SPI_VFD/blob/master/examples/createChar/createChar.pde
+
+ This example code is in the public domain.
+ http://www.arduino.cc/en/Tutorial/LiquidCrystal
+
+ Also useful:
+ http://icontexto.com/charactercreator/
+
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int i ;
+// make some custom characters:
+byte heart[8] = {
+  0b00000,
+  0b01010,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b00000
+};
+
+byte smiley[8] = {
+  0b00000,
+  0b00000,
+  0b01010,
+  0b00000,
+  0b00000,
+  0b10001,
+  0b01110,
+  0b00000
+};
+
+byte frownie[8] = {
+  0b00000,
+  0b00000,
+  0b01010,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b01110,
+  0b10001
+};
+
+byte armsDown[8] = {
+  0b00100,
+  0b01010,
+  0b00100,
+  0b00100,
+  0b01110,
+  0b10101,
+  0b00100,
+  0b01010
+};
+
+byte armsUp[8] = {
+  0b00100,
+  0b01010,
+  0b00100,
+  0b10101,
+  0b01110,
+  0b00100,
+  0b00100,
+  0b01010
+};
+
+void setup() {
+  // initialize LCD and set up the number of columns and rows:
+  lcd.begin(16, 2);
+
+  // create a new character
+  lcd.createChar(0, heart);
+  // create a new character
+  lcd.createChar(1, smiley);
+  // create a new character
+  lcd.createChar(2, frownie);
+  // create a new character
+  lcd.createChar(3, armsDown);
+  // create a new character
+  lcd.createChar(4, armsUp);
+lcd.setCursor(0, 0);
+  // Print a message to the lcd.
+  lcd.print("I ");
+  lcd.write(byte(0)); // when calling lcd.write() '0' must be cast as a byte
+  lcd.print(" Arduino! ");
+  lcd.write((byte) 1);
+delay(1000);
+  lcd.clear();
+}
+
+void loop()
+{
+  lcd.clear();
+  for (i=0;i<8;i++)
+  {
+    lcd.clear();
+    if ((i%2)==0)
+    {
+      lcd.setCursor(7-i, 0);
+      lcd.write(3);
+      lcd.setCursor(i+8, 0);
+      lcd.write(3);
+    }
+    else
+    {
+      lcd.setCursor(7-i, 0);
+      lcd.write(4);
+      lcd.setCursor(i+8, 0);
+      lcd.write(4);
+    }
+    delay(500);
+  }
+
+  for (i=0;i<8;i++)
+  {
+    lcd.clear();
+    if ((i%2)==0)
+    {
+      lcd.setCursor(i, 1);
+      lcd.write(3);
+      lcd.setCursor(15-i, 1);
+      lcd.write(3);
+    }
+    else
+    {
+      lcd.setCursor(i, 1);
+      lcd.write(4);
+      lcd.setCursor(15-i, 1);
+      lcd.write(4);
+    }
+    delay(500);
+  }
+
+  
+}
---
+++ Code_011_LCD_Character/LCD_Character4/TextDirection2/TextDirection2.ino
@@ -0,0 +1,34 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int str[] = {
+  'S', 'a', 'n', 'd', 'w', 'i','c','h'
+};
+int i ;
+void setup() {
+  lcd.begin(16, 2);
+//  lcd.cursor();
+}
+void loop() {
+  lcd.home();
+  lcd.clear();
+  lcd.leftToRight();
+  for(i=0;i<8;i++)
+  {
+    lcd.write(str[i]);delay(300);
+  }
+  lcd.clear();
+  lcd.setCursor(7, 0);
+  lcd.rightToLeft();
+  for(i=8;i>0;i--)
+  {
+    lcd.write(str[i-1]);delay(300);
+  }
+}
+
+
+
+
+
+
+
+
