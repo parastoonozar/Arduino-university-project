bd081d2ac890419597e263fcf56eba65f642337c
parastoonozar <148582607+parastoonozar@users.noreply.github.com>
2024-01-01 18:13:23 +0330

    Add files via upload

---
+++ Code_004_Input_Button/Code_004_Input_Button.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(7, INPUT_PULLUP);
+}
+char i ;
+void loop() {
+  // put your main code here, to run repeatedly:
+    if( digitalRead(7))
+    {
+      digitalWrite(13,LOW);
+    }
+    else
+    {
+      digitalWrite(13,HIGH);
+    }
+}
